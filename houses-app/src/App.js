dedd9c8b1b645eeb2b78865bf9d3776a4830f91b
JasonNaggles <JasonNaggles.verizon.net>
2023-09-28 18:29:49 -0400

    first commit

--- houses-app/src/App.js
+++ houses-app/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HousesList props={{test: data}}
+      </div>
+    )
+  }
 }
-
-export default App;
