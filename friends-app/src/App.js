1116a46c5feebf84e10b626880d063f029a4180d
JasonNaggles <JasonNaggles.verizon.net>
2023-10-06 23:25:16 -0400

    first commit

--- friends-app/src/App.js
+++ friends-app/src/App.js
@@ -2,32 +2,37 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  
+  // Define the mock API URL as a constant 
 const MOCK_API_URL = "https://650fc3383ce5d181df5ca880.mockapi.io/Friends";  
   
-const [newFirstName, setNewFirstName] = useState('')
-const [newLastName, setNewLastName] = useState('')
-const [friends, setFriends] = useState([])
-const [updatedFirstName, setUpdatedFirstName] = useState('')
-const [updatedLastName, setUpdatedLastName] = useState('')
+// Define state variables using the useState hook
+const [newFirstName, setNewFirstName] = useState('') // Input field for new first name
+const [newLastName, setNewLastName] = useState('') // Input field for new last name
+const [friends, setFriends] = useState([]) // Array to store friends from the API
+const [updatedFirstName, setUpdatedFirstName] = useState('') // Input field for updated first name
+const [updatedLastName, setUpdatedLastName] = useState('') // Input field for updated last name
 
-  
+// Function to fetch friends data from the API
+
 function getFriends() {
   fetch(MOCK_API_URL)
   .then(data => data.json())
-  .then(data => setFriends(data))
+  .then(data => setFriends(data)) // Update the friends state with data from the API
 }
 
+// Use the useEffect hook to fetch friends data when the component mounts
 useEffect(() => {
   getFriends()
 }, [])
 
+// Function to delete a friend by ID
 function deleteFriends(id) {
   fetch(`${MOCK_API_URL}/${id}`, {
     method: 'DELETE',
-  }).then(() => getFriends())
+  }).then(() => getFriends()) // Fetch friends data again after deletion
 }
 
+// Function to add a new friend
 function postNewFriends(e) {
   e.preventDefault()
   fetch(MOCK_API_URL, {
@@ -37,9 +42,10 @@ function postNewFriends(e) {
       firstName: newFirstName,
       lastName: newLastName,
     })
-  }).then(() => getFriends())
+  }).then(() => getFriends()) // Fetch friends data again after adding a new friend
 }
 
+// Function to update a friend's information
 function updateFriends(e, friendsObject) {
   e.preventDefault()
   let updatedFriendsObject = {
@@ -52,7 +58,7 @@ function updateFriends(e, friendsObject) {
     body: JSON.stringify(updatedFriendsObject),
     headers: {
       "Content-Type": "application/json"},
-    }).then(() => getFriends())
+    }).then(() => getFriends()) // Fetch friends data again after updating a friend
 
   }
 
